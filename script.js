4e83a6a5564387118cea7cd4b31cca9e119ca26a
JOSEALM3IDA <jfeaisec@gmail.com>
2019-12-22 22:24:29 +0000

    pqp nao consequi fazer a animaçao

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 defineEvents(); //define events
 
-function defineEvents(){
-    let arrow = document.getElementById("mobile-arrow")
+function defineEvents() {
+  let arrow = document.getElementById("mobile-arrow");
 
-    arrow.addEventListener("click", dropMenu)
+  arrow.addEventListener("click", dropMenu);
 }
 
-function dropMenu(){
-    if (this.style.transform == "scaleY(-1)"){
-        this.style.transform = "scaleY(1)";
+function dropMenu() {
+  if (this.style.transform == "scaleY(-1)") {
+    this.style.transform = "scaleY(1)";
 
-        document.getElementById("navbar-inside").style.display = "none";
-    }
-    else {
-        this.style.transform = "scaleY(-1)";
+    document.getElementById("navbar-inside").style.display = "none";
+  } else {
+    this.style.transform = "scaleY(-1)";
 
-        document.getElementById("navbar-inside").style.display = "inline";
-    }
+    document.getElementById("navbar-inside").style.display = "inline";
+  }
 }
