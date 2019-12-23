8873a706999a42d4d1d5b7687866ed040b35d73d
JOSEALM3IDA <jfeaisec@gmail.com>
2019-12-23 02:32:18 +0000

    seta ficou fdd, revê. slideshow done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-defineEvents(); //define events
-
 function defineEvents() {
   let arrow = document.getElementById("mobile-arrow");
+  let navbar = document.getElementById("navbar-inside");
+  let slide_img = document.getElementById("showcase");
 
   arrow.addEventListener("click", dropMenu);
 }
@@ -10,10 +10,36 @@ function dropMenu() {
   if (this.style.transform == "scaleY(-1)") {
     this.style.transform = "scaleY(1)";
 
-    document.getElementById("navbar-inside").style.display = "none";
+    navbar.style.display = "none";
   } else {
     this.style.transform = "scaleY(-1)";
 
-    document.getElementById("navbar-inside").style.display = "inline";
+    navbar.style.display = "inline";
   }
 }
+
+// Declaralão de variaveis para o slideshow
+var index_slide_img = 0;
+var slide_imgs = 3; // Numero de fotos a apresentar no slideshow
+
+function slideshow(){
+  index_slide_img++;
+  if(index_slide_img == 3){
+    index_slide_img = 0;
+    showcase.style.background =  "url('../img/slide-"+index_slide_img+".jpg') no-repeat center center/cover";
+  } 
+  else{
+    showcase.style.background =  "url('../img/slide-"+index_slide_img+".jpg') no-repeat center center/cover";
+  }
+
+  setTimeout(slideshow,5000);
+}
+
+function init(){
+  showcase.style.background =  "url('../img/slide-"+index_slide_img+".jpg') no-repeat center center/cover";
+
+  setTimeout(slideshow,5000);
+}
+
+defineEvents(); //define events
+init(); //inicializaçao de certos aspetos da pagina
