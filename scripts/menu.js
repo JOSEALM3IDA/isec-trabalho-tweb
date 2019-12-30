f49ce022b58873abd3a14cd3354b391bd01fbf37
JOSEALM3IDA <jfeaisec@gmail.com>
2019-12-30 13:40:28 +0000

    quality of life

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,5 @@
 function defineEvents() {
-  let arrow = document.getElementById("mobile-arrow");
+  let arrow = document.getElementById("mobile_arrow");
 
   arrow.addEventListener("click", dropMenu);
 }
@@ -13,14 +13,12 @@ function dropMenu() {
     for (i = 0; i < navbar.length; i++) {
       navbar[i].style.display = "none";
     }
-    logo.style.display = "block";
   } else {
     this.style.transform = "scaleY(-1)";
 
     for (i = 0; i < navbar.length; i++) {
       navbar[i].style.display = "inline";
     }
-    logo.style.display = "block";
   }
 }
 
