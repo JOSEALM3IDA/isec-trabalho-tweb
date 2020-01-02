c84dd246c921bb50c29d61f5bf0e5b9b4eb3a57b
JOSEALM3IDA <jfeaisec@gmail.com>
2020-01-02 21:06:22 +0000

    aa

---
+++ scripts/projetos.js
@@ -0,0 +1,52 @@
+function defineEvents() {
+  let info = document.getElementById("mais-projetos");
+  let projetos = document.getElementsByClassName("mais-projetos");
+
+  item0.addEventListener("click", function() {
+    info.scrollIntoView();
+    projetos[0].style.display = "block";
+  });
+  item1.addEventListener("click", function() {
+    info.scrollIntoView();
+  });
+  item2.addEventListener("click", function() {
+    info.scrollIntoView();
+  });
+  item3.addEventListener("click", function() {
+    info.scrollIntoView();
+  });
+  item4.addEventListener("click", function() {
+    info.scrollIntoView();
+  });
+  item5.addEventListener("click", function() {
+    info.scrollIntoView();
+  });
+  item6.addEventListener("click", function() {
+    info.scrollIntoView();
+  });
+  item7.addEventListener("click", function() {
+    info.scrollIntoView();
+  });
+  item8.addEventListener("click", function() {
+    info.scrollIntoView();
+  });
+  item9.addEventListener("click", function() {
+    info.scrollIntoView();
+  });
+  item10.addEventListener("click", function() {
+    info.scrollIntoView();
+  });
+  item11.addEventListener("click", function() {
+    info.scrollIntoView();
+  });
+  item12.addEventListener("click", function() {
+    info.scrollIntoView();
+  });
+  item13.addEventListener("click", function() {
+    info.scrollIntoView();
+  });
+
+  console.log(projetos);
+}
+
+defineEvents();
