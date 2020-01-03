f3f4d3f3eb3836b3c74577f82a58c9947c911778
JOSEALM3IDA <jfeaisec@gmail.com>
2020-01-03 10:01:28 +0000

    mapas

--- scripts/projetos.js
+++ scripts/projetos.js
@@ -2,22 +2,43 @@ function defineEvents() {
   let info = document.getElementById("mais-projetos");
   let projetos = document.getElementsByClassName("mais-projetos");
   let close = document.getElementsByClassName("close");
+  let maps = document.getElementsByClassName("map");
   let cerasMap = document.getElementById("maps-ceras");
+  let crasmMap = document.getElementById("maps-crasm");
+  let crassaMap = document.getElementById("maps-crassa");
   let ceras = document.getElementById("ceras");
+  let crasm = document.getElementById("crasm");
+  let crassa = document.getElementById("crassa");
   var nCells = 14; // Numero de cells no grid
 
   ceras.addEventListener("click", function() {
     cerasMap.style.display = "block";
+    crasmMap.style.display = "none";
+    crassaMap.style.display = "none";
+  });
+
+  crasm.addEventListener("click", function() {
+    cerasMap.style.display = "none";
+    crasmMap.style.display = "block";
+    crassaMap.style.display = "none";
+  });
+
+  crassa.addEventListener("click", function() {
+    cerasMap.style.display = "none";
+    crasmMap.style.display = "none";
+    crassaMap.style.display = "block";
   });
 
   for (i = 0; i < nCells; i++) {
     document.getElementById("item" + i).addEventListener("click", function() {
       info.scrollIntoView();
 
-      cerasMap.style.display = "none"; //temp fix
       global = this.id.slice(4);
       for (j = 0; j < nCells; j++) {
         projetos[j].style.display = "none";
+        if (j < maps.length) {
+          maps[j].style.display = "none"; // Fechar todos os mapas
+        }
       }
       projetos[global].style.display = "block";
       close[global].addEventListener("click", function() {
